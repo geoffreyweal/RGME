ceeab92aa7be697b4dfd2a286e7f954b06e39c75
Geoffrey Weal <geoffrey.weal@gmail.com>
2025-04-29 15:56:28 +1200

    Update setup_GauOpen.sh

--- setup_GauOpen.sh
+++ setup_GauOpen.sh
@@ -5,13 +5,13 @@
 module purge
 
 # Load python and gcc modules (This is specifically for Rapoi VUW computer cluster)
-#module load GCCcore/5.4.0
-#module load GCC/5.4.0
-#module load python/3.6.8
+module load GCCcore/5.4.0
+module load GCC/5.4.0
+module load python/3.6.8
 
-module load GCCcore/8.2.0
-module load GCC/8.2.0
-module load python/3.7.3
+#module load GCCcore/8.2.0
+#module load GCC/8.2.0
+#module load python/3.7.3
 
 #module load GCCcore/10.3.0
 #module load GCC/10.3.0
